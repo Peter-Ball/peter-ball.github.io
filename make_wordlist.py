2f63e0abca7cb4b0276635caf73f06c785b29615
Peter Ball <peterball12@gmail.com>
2022-10-16 10:36:19 -0400

    new formula for deriving the wordlist

---
+++ make_wordlist.py
@@ -0,0 +1,34 @@
+# Requires COMMON.TXT from Project Gutenberg Moby II
+
+import re
+
+def main():
+
+    # loading
+
+    with open("COMMON.TXT") as fp:
+        common = fp.read().split()
+
+    with open("bad-words.txt") as fp:
+        bad_words = fp.read().split()
+
+    # cleaning
+
+    unwanted_chars = ['.', '-', ' ']
+    regex = re.compile(r'\.|-| |[A-Z]')
+    common = [word for word in common if not regex.search(word)]
+
+    common = [word for word in common if len(word) <= 8 and len(word) >= 5]
+
+    common = list(set(common) - set(bad_words))
+
+    print(len(common))
+
+    with open("wordlist.txt", 'w') as fp:
+        fp.write('\n'.join(common))
+
+
+
+
+if __name__ == "__main__":
+    main()
